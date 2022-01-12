aa5dddc653bf5c60faa47e9f5d7492dc9911ce92
vagrant <vagrant@homestead>
2022-01-12 06:24:29 +0000

    商品模型

---
+++ database/migrations/2022_01_12_060620_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->comment('商品名称');
+            $table->text('description')->comment('商品描述');
+            $table->string('image');
+            $table->boolean('on_sale')->default(true);
+            $table->float('rating')->default(5);
+            $table->unsignedInteger('sold_count')->default(0)->comment('销量');
+            $table->unsignedInteger('review_count')->default(0)->comment('评论数');
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
