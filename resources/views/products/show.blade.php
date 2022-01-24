155dc5a36224f9bc34ac896dc7f571f57d91e9c8
vagrant <vagrant@homestead>
2022-01-24 02:25:42 +0000

    头部添加购物车图标

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -114,7 +114,10 @@
           amount: $(".cart_amount input").val(),
         })
         .then(function(){//请求成功
-          swal('加入购物车成功', '', 'success');
+          swal('加入购物车成功', '', 'success')
+          .then(function(){
+            location.href='{{ route('cart.index') }}';
+          });
         }, function(error){//请求失败
           if(error.response.status===401){
             //用户未登录
