19141a9397322feb7f35791cdffe115106fc872c
ery.he <ery.he@feisu.com>
2022-01-28 09:45:53 +0000

    创建订单

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -34,10 +34,17 @@ class OrderRequest extends Request
                         return $fail('该商品已售完');
                     }
                     //获取当前索引
-
+                    preg_match('/items\.(\d+)\.sku_id/', $attribute, $m);
+                    $index = $m[1];
+                    //根据索引找到用户所提交的购买数量
+                    $amount = $this->input('items')[$index]['amount'];
+                    if($amount >0 && $amount > $sku->stock){
+                        return $fail('该商品库存不足');
+                    }
                 },
 
-            ]
+            ],
+            'items.*.amount' => ['required', 'integer', 'min:1'],
         ];
     }
 }
